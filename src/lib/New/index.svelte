0683eaa764a01ca935fb149c2f7fe4baf8a69e70
Jaime López <5743857+loama@users.noreply.github.com>
2022-11-28 20:18:29 -0600

    add codemirror

--- src/lib/New/index.svelte
+++ src/lib/New/index.svelte
@@ -1,9 +1,25 @@
 <script>
+  import CodeMirror, { basicSetup } from "./codemirror.svelte";
+  import { javascript } from "@codemirror/lang-javascript";
+  import { EditorState } from "@codemirror/state";
+  let store;
+
+  function changeHandler({ detail: { tr } }) {
+    console.log("change", tr.changes.toJSON());
+  }
 </script>
 
 <div class="h-full flex">
-  <div class="box-border border-r border-zinc-200 h-full w-1/2">
+  <div
+    class="box-border border-r border-zinc-200 h-full w-1/2 overflow-y-scroll"
+  >
     CODE RESULT
+    <CodeMirror
+      doc={"var c = document.getElementById('myCanvas');\nvar ctx = c.getContext('2d');"}
+      bind:docStore={store}
+      extensions={[basicSetup, javascript(), EditorState.readOnly.of(true)]}
+      on:change={changeHandler}
+    />
   </div>
 
   <div class="border-r border-zinc-200 h-full w-1/2">
